9c0088fd1228250f27f8c68af7a81034ab0875c0
Kishan Changani <kishank.changani1312@gmail.com>
2022-03-12 12:13:55 +0530

    changesinurl

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://react.js"
+          href="httpl"
           target="_blank"
           rel="noopener noreferrer"
         >
